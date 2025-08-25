56c87c1ee1f667440e48ad28895e7a7f42e21668
Amirabbas <amirabbas.linux@gmail.com>
2025-08-25 04:13:30 +0330

    fix some ui issue

--- app/profile/allcourses/page.jsx
+++ app/profile/allcourses/page.jsx
@@ -303,7 +303,7 @@ function AllCourses() {
                               >
                                 <button
                                   onClick={() => setOpenTermsBatchId(null)}
-                                  className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+                                  className="absolute top-4 left-4 text-gray-500 hover:text-gray-700"
                                 >
                                   <IoClose className="text-2xl" />
                                 </button>
--- components/profile/EditModal.jsx
+++ components/profile/EditModal.jsx
@@ -18,7 +18,7 @@ const EditModal = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 backdrop-blur-3xl w-5/6 flex items-center justify-center z-50" dir="rtl">
+    <div className="fixed inset-0 backdrop-blur-3xl flex items-center justify-center z-50" dir="rtl">
       <div className="bg-white p-6 rounded-lg max-w-md w-full max-h-[90vh] overflow-y-auto shadow-2xl">
         <h2 className="text-xl text-black font-bold mb-4 text-center">{title}</h2>
         <form onSubmit={onSave} className="space-y-4">
